f23fc18e71158d94370275aabd403aa75a9418c5
shubham31011998 <shubhamyaduvanshi03@gmail.com>
2023-06-10 12:58:06 +0530

    initial code

---
+++ app/posts/[postid]/postid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const postid = () => {
+  return (
+    <div>postid</div>
+  )
+}
+
+export default postid
---
+++ app/posts/newpost/newpost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newpost = () => {
+  return (
+    <div>newpost</div>
+  )
+}
+
+export default newpost
